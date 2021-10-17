b00854d2ff4caa3c419a6f869be4cef07ccaced0
Oluwasegun Coker <cokeroluwasegun@outlook.com>
2021-10-17 02:00:08 +0100

    Created directory and files and this is the first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a madLib for python project
+# It is a game where you enter random words
+# and number to fill in the gap.
+
+word = input("Enter your first random word: ")
+word2 = input("Enter your second random word: ")
+word3 = input("Enter your third random word: ")
+num = input("Enter number: ")
+
+
+madLib = f"The world is full of {word} people who now see kindness as been {word2} and \
+{word3} as been real. God is the {num}\n"
+print(madLib)
